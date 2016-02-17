f83fcfeefecf1bc5bdc279e7727d2eb0c007bd94
Michał Kałużny <maku@justmaku.org>
2016-02-17 01:04:47 +0100

    Added dotfile update support

--- update.sh
+++ update.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 
+#=========
+# Dotfiles
+#=========
+
+cp ~/.zshrc dotfiles
+cp ~/.bash_profile dotfiles
+
+#=====
+# Brew
+#=====
+
 brew list | sed > dependencies/brew
 brew cask list | sed > dependencies/cask
+
+#====
+# Gem
+#====
+
 gem list --local --no-versions --no-verbose | tail -n +1 > dependencies/gem
+
+#=============
+# Sublime Text
+#=============
+
 cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings dependencies/sublime-packages
 cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings blobs/sublime-settings
